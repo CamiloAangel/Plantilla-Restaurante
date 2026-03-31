35857f7eadd38c54d94fc05633fbda3a84e80878
CamiloAangel <angelcami0912@gmail.com>
2026-03-30 22:39:46 -0500

    Ajuste Index.ts inventario

--- app/components/admin/index.ts
+++ app/components/admin/index.ts
@@ -1,4 +1,4 @@
 export { default as DashboardSection } from './DashboardSection';
 export { default as SidebarAdmin } from './SidebarAdmin';
-export { default as InventorySection } from './InventorySection';
+export { default as InventorySection } from '../inventory/InventorySection';
 export { default as StaffSection } from './StaffSection';
